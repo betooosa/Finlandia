b83be95aeeb3dfc2ab1cd74228080030706a26bc
Beto Sanchez <betooosao78@gmail.com>
2023-03-08 17:29:04 -0600

    Modificado

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,11 @@ def suma(nim1, nume):
     total = nim1 + nume
     return total
 
+def resta(nim1, nume):
+    total = nim1 - nume
+    return total
+
 resultado = suma(nim1, nume)
+resultado = resta(nim1, nume)
+
 print('El resultado es:',resultado)
